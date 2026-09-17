e15af4904934be94129b705ed52cbb8bee2e021c
Nutthawut Srinamol <oat19454@gmail.com>
2026-09-17 20:17:59 +0700

    install next-auth

---
+++ web/lib/auth/auth-options.ts
@@ -0,0 +1,12 @@
+import type { NextAuthOptions } from "next-auth";
+import KeycloakProvider from "next-auth/providers/keycloak";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    KeycloakProvider({
+      clientId: process.env.KEYCLOAK_CLIENT_ID!,
+      clientSecret: process.env.KEYCLOAK_CLIENT_SECRET!,
+      issuer: process.env.KEYCLOAK_ISSUER!,
+    }),
+  ],
+};
